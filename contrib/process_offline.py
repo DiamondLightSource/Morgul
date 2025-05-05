b29d50fe9955685d0287be952c3865810f134f53
Nicholas Devenish <ndevenish@gmail.com>
2025-05-05 19:02:22 +0100

    Split source into processes; causes failures

--- contrib/process_offline.py
+++ contrib/process_offline.py
@@ -12,10 +12,12 @@
 # ///
 import contextlib
 import json
+import multiprocessing
 import shlex
 import shutil
 import subprocess
 import sys
+import threading
 import time
 from argparse import ArgumentParser
 from concurrent.futures import ProcessPoolExecutor
@@ -27,7 +29,6 @@ import hdf5plugin  # noqa: F401
 import numpy as np
 import zmq
 from rich import print
-from tqdm import tqdm
 
 # Basic ports for internal send/recv
 SEND_PORT = 30001
@@ -36,6 +37,37 @@ h5_compression = {"compression": 32008, "compression_opts": (0, 2)}
 
 MORGUL = shutil.which("morgul-cuda") or "/scratch/nickd/morgul/_build/morgul-cuda"
 
+# Common header, for all the common/unused fields
+BASE_HEADER = {
+    "acqIndex": 0,
+    "bitmode": 16,
+    "column": 0,
+    "completeImage": 0,
+    "data": 0,
+    "detshape": [1, 4],
+    "detSpec1": 0,
+    "detSpec2": 0,
+    "detSpec3": 0,
+    "detSpec4": 0,
+    "detType": 0,
+    "expLength": 0.0,
+    "fileIndex": 0,
+    "flipRows": 0,
+    "fname": "",
+    "frameIndex": 0,
+    "frameNumber": 0,
+    "jsonversion": 0,
+    "modId": 0,
+    "packetNumber": 0,
+    "progress": 0.0,
+    "quad": 0,
+    "row": 0,
+    "shape": [1024, 256],
+    "size": 0,
+    "timestamp": 0,
+    "version": 0,
+}
+
 
 class FileInfo(NamedTuple):
     pos: tuple[int, int]
@@ -43,15 +75,6 @@ class FileInfo(NamedTuple):
     filename: Path
     exptime: float
     gainmode: str
-    file: h5py.File
-    data: h5py.Dataset
-
-
-def start_sender(port: int) -> zmq.Socket:
-    context = zmq.Context()
-    socket = context.socket(zmq.PUB)
-    socket.bind(f"tcp://localhost:{port}")
-    return socket
 
 
 class DataChunker:
@@ -75,17 +98,15 @@ class DataChunker:
 
 
 def read_h5_file(file: Path) -> FileInfo:
-    f = h5py.File(file)
-    assert f["data"].shape[1:] == (256, 1024), f"Unexpected data shape in {file}"
-    return FileInfo(
-        pos=(int(f["row"][()]), int(f["column"][()])),
-        frames=f["data"].shape[0],
-        filename=file,
-        exptime=float(f["exptime"][()]),
-        gainmode=f["gainmode"][()],
-        file=f,
-        data=f["data"],
-    )
+    with h5py.File(file) as f:
+        assert f["data"].shape[1:] == (256, 1024), f"Unexpected data shape in {file}"
+        return FileInfo(
+            pos=(int(f["row"][()]), int(f["column"][()])),
+            frames=f["data"].shape[0],
+            filename=file,
+            exptime=float(f["exptime"][()]),
+            gainmode=f["gainmode"][()],
+        )
 
 
 def only(items):
@@ -148,6 +169,87 @@ def run_sink(port: int, output: Path, common: Common, *, ignore: int = 0):
             data.id.write_direct_chunk((i, 0, 0), frame)
 
         print(f"Received all images on sink port {port}")
+
+
+def run_source(
+    port: int,
+    pedestal_file: Path,
+    data_file: Path,
+    common: Common,
+    pedestal_frames: int,
+    pedestal_loops: int,
+    acquisition_barrier: threading.Barrier,
+):
+    print(f"Running source: {port}")
+    # def start_sender(port: int) -> zmq.Socket:
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind(f"tcp://localhost:{port}")
+
+    # Wait until we are all ready to send
+    acquisition_barrier.wait()
+
+    collection_info = json.loads(
+        (data_file.parent / "collection_info.json").read_bytes()
+    )
+    with h5py.File(pedestal_file) as pedestal:
+        num_images_pedestal = pedestal["data"].shape[0]
+        for i in range(num_images_pedestal):
+            frame_data = pedestal["data"][i].tobytes()
+
+            header = BASE_HEADER | {
+                "acqIndex": 1,
+                "shape": [1024, 256],
+                "detshape": [1, 4],
+                "row": int(pedestal["row"][()]),
+                "column": int(pedestal["column"][()]),
+                "expLength": common.exptime,
+                "frameNumber": i,
+                "frameIndex": i,
+                "bitmode": 16,
+                "progress": 100 * i / num_images_pedestal,
+                "addJsonHeader": {
+                    "pedestal": True,
+                    "pedestal_frames": pedestal_frames,
+                    "pedestal_loops": pedestal_loops,
+                    "wavelength": str(collection_info["wavelength"]),
+                },
+                "timestamp": int(time.time()),
+            }
+            socket.send_multipart([json.dumps(header).encode(), frame_data])
+
+    socket.send(json.dumps(BASE_HEADER | {"bitmode": 0}).encode())
+
+    # Wait until we have all sent all of the pedestal data
+    acquisition_barrier.wait()
+
+    print("Send all pedestal data, now sending measurement data")
+    time.sleep(2)
+
+    with h5py.File(data_file) as data:
+        num_images_data = data["data"].shape[0]
+        for i in range(num_images_data):
+            frame_data = data["data"][i].tobytes()
+
+            header = BASE_HEADER | {
+                "acqIndex": 2,
+                "shape": [1024, 256],
+                "detshape": [1, 4],
+                "row": int(data["row"][()]),
+                "column": int(data["column"][()]),
+                "expLength": common.exptime,
+                "frameNumber": i + num_images_pedestal,
+                "frameIndex": i,
+                "bitmode": 16,
+                "progress": 100 * i / num_images_data,
+                "addJsonHeader": {
+                    "wavelength": str(collection_info["wavelength"]),
+                },
+                "timestamp": int(time.time()),
+            }
+            socket.send_multipart([json.dumps(header).encode(), frame_data])
+
+        socket.send(json.dumps(BASE_HEADER | {"bitmode": 0}).encode())
 
 
 def run():
@@ -232,9 +334,9 @@ def run():
     # print(matched_data)
 
     # Make the sending sockets
-    senders = [
-        start_sender(x) for x in range(SEND_PORT, SEND_PORT + len(pedestal_info))
-    ]
+    # senders = [
+    #     start_sender(x) for x in range(SEND_PORT, SEND_PORT + len(pedestal_info))
+    # ]
 
     sink_pool = ProcessPoolExecutor(len(pedestal_info))
     for port in range(RECV_PORT, RECV_PORT + len(pedestal_info)):
@@ -246,108 +348,48 @@ def run():
         "--detector",
         args.detector,
         "live",
-        "--no-progress",
+        # "--no-progress",
         "--zmq-host",
         "localhost",
         "--zmq-port",
         str(SEND_PORT),
-        str(len(senders)),
+        str(len(pedestal_info)),
     ]
     print("+ " + shlex.join(cmd))
     morgul_live = subprocess.Popen(args=cmd)
     # Wait for this to be ready.. Just a guess as ZMQ makes this a bit opaque
-    time.sleep(2)
+    time.sleep(3)
+    print("\nStarting submitter processes...")
 
-    base_header = {
-        "acqIndex": 0,
-        "bitmode": 16,
-        "column": 0,
-        "completeImage": 0,
-        "data": 0,
-        "detshape": [1, 4],
-        "detSpec1": 0,
-        "detSpec2": 0,
-        "detSpec3": 0,
-        "detSpec4": 0,
-        "detType": 0,
-        "expLength": 0.0,
-        "fileIndex": 0,
-        "flipRows": 0,
-        "fname": "",
-        "frameIndex": 0,
-        "frameNumber": 0,
-        "jsonversion": 0,
-        "modId": 0,
-        "packetNumber": 0,
-        "progress": 0.0,
-        "quad": 0,
-        "row": 0,
-        "shape": [1024, 256],
-        "size": 0,
-        "timestamp": 0,
-        "version": 0,
-    }
+    barrier = multiprocessing.Barrier(len(pedestal_info))
+
+    sources: list[multiprocessing.Process] = []
+    for i, port in enumerate(range(SEND_PORT, SEND_PORT + len(pedestal_info))):
+        print(f"Submitting {i}")
+
+        process = multiprocessing.Process(
+            target=run_source,
+            args=(
+                port,
+                pedestal_info[send_order[i]].filename,
+                data_info[send_order[i]].filename,
+            ),
+            kwargs={
+                "common": common,
+                "pedestal_frames": args.frames,
+                "pedestal_loops": args.loops,
+                "acquisition_barrier": barrier,
+            },
+        )
+        sources.append(process)
+        process.start()
+
     try:
-        # Send all the pedestal data
-        for i in tqdm(range(pedestal_frames), desc="Pedestals"):
-            for sock, ped in zip(senders, [pedestal_info[x] for x in send_order]):
-                frame_data = ped.data[i].tobytes()
-                header = base_header | {
-                    "acqIndex": 1,
-                    "shape": [1024, 256],
-                    "detshape": [1, 4],
-                    "row": ped.pos[0],
-                    "column": ped.pos[1],
-                    "expLength": common.exptime,
-                    "frameNumber": i,
-                    "frameIndex": i,
-                    "bitmode": 16,
-                    "progress": 100 * i / pedestal_frames,
-                    "addJsonHeader": {
-                        "pedestal": True,
-                        "pedestal_frames": args.frames,
-                        "pedestal_loops": args.loops,
-                        "wavelength": str(collection_info["wavelength"]),
-                    },
-                    "timestamp": int(time.time()),
-                }
-                sock.send_multipart([json.dumps(header).encode(), frame_data])
-
-        # Send the end packets
-        for sock in senders:
-            sock.send(json.dumps(base_header | {"bitmode": 0}).encode())
-
-        print("Send all pedestal data, now sending measurement data")
-        time.sleep(2)
-
-        # Now send the data frames
-        for i in tqdm(range(data_frames), desc="Data"):
-            for sock, data in zip(senders, [data_info[x] for x in send_order]):
-                # print(i, data)
-                frame_data = data.data[i].tobytes()
-                header = base_header | {
-                    "acqIndex": 2,
-                    "shape": [1024, 256],
-                    "detshape": [1, 4],
-                    "row": data.pos[0],
-                    "column": data.pos[1],
-                    "expLength": common.exptime,
-                    "frameNumber": i + pedestal_frames,
-                    "frameIndex": i,
-                    "bitmode": 16,
-                    "progress": 100 * i / data_frames,
-                    "addJsonHeader": {
-                        "wavelength": str(collection_info["wavelength"]),
-                    },
-                    "timestamp": int(time.time()),
-                }
-                sock.send_multipart([json.dumps(header).encode(), frame_data])
-
-        # Send the end packets
-        for sock in senders:
-            sock.send(json.dumps(base_header | {"bitmode": 0}).encode())
-
         # Wait for all sinks to be done
+        print("Waiting for sending to be complete...")
+        for source in sources:
+            source.join()
+
         print("Waiting to finish receiving all data")
         sink_pool.shutdown(wait=True)
 
